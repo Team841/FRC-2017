ebb089a658d6dbbf15468d5a02b9fa4f5aaf6bf4
Team841 <josh.quintero@gmail.com>
2017-02-02 17:17:32 -0800

    Contansts isolated in only one file
    All the constants are now in Constants.java.

---
+++ Waffle/src/org/usfirst/frc841/Waffle/Constants.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.usfirst.frc841.Waffle;
+
+/**
+ * In this class we are storing all the constants and tunings used in all the commands and subsystems
+ * @author Team841-P01
+ *
+ */
+public class Constants {
+
+	private Constants(){
+		// Prevents instantiation
+	}
+
+	// STRUCTURE OF THE CONSTANTS CONTAINED IN THIS DOCUMENT
+	//public static final double test = 10.001;	
+	
+	// CONSTANTS FOR THE DRIVETRAIN
+	public static final double throttleDeadband = 0.02; 
+	public static final double wheelDeadband = 0.02;	
+	public static final double sensitivityHigh = 0.85;	
+	public static final double sensitivityLow = 0.75;	
+	
+}
--- Waffle/src/org/usfirst/frc841/Waffle/commands/Drive.java
+++ Waffle/src/org/usfirst/frc841/Waffle/commands/Drive.java
@@ -37,6 +37,7 @@ public class Drive extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.driveTrain.InitCompressor();
     }
 
     // Called repeatedly when this Command is scheduled to run
