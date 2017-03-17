7cc6c2275e9c640bd7c5bd2e7b7709bf684c1dd7
Team841 <josh.quintero@gmail.com>
2017-03-17 12:11:41 -0700

    Autonomous to the Left and Right (alpha)

--- Waffle/src/org/usfirst/frc841/Waffle/Constants.java
+++ Waffle/src/org/usfirst/frc841/Waffle/Constants.java
@@ -24,7 +24,9 @@ public class Constants {
 	public static final double sensitivityLow = 0.75;
 	public static final double centervalue = 80;
 	public static final double tolerance = 10;
-	public static final double autoOffSet = 5; 
+	//public static final double autoOffSet = 5; 
+	public static final double autoDrivingDistance = 4.2; // In time, is seconds driving from the starting point (SQRT of 2 two times and multiplied by the side of the driving straight)
+	public static final double autoTurningDistance = .8; // In time, is seconds turning to point to the peg of the sides	
 	//public static int nAutonomousMode = 0;
 	
 }
